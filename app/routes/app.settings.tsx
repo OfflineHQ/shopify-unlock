5352881c5f4cc9056679be8db16c49a24fcc8a81
sebpalluel <sebpalluel@gmail.com>
2024-06-12 18:45:14 +0200

    ♻️ (app.create-campaign.tsx): refactor UI components for clarity and maintainability 🐛 (app.create-campaign.tsx): fix form validation logic to ensure correct behavior ✨ (app.settings.tsx): add dynamic form handling for multiple languages 📝 (app.settings.tsx): update documentation within code to reflect changes and enhancements

--- app/routes/app.settings.tsx
+++ app/routes/app.settings.tsx
@@ -1,15 +1,19 @@
 import { json, type LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { Form, useFetcher, useLoaderData } from "@remix-run/react";
 import {
-  BlockStack,
   Box,
+  Button,
   Card,
+  FormLayout,
   Layout,
-  Link,
-  List,
   Page,
+  PageActions,
+  Tabs,
   Text,
+  TextField
 } from "@shopify/polaris";
+import { useForm, useList } from "@shopify/react-form";
+import { useCallback, useState } from "react";
 import { authenticate } from "~/shopify.server";
 
 export const loader = async ({ request }: LoaderFunctionArgs) => {
@@ -30,12 +34,81 @@ export const loader = async ({ request }: LoaderFunctionArgs) => {
   const responseJson = await response.json();
   const languages = responseJson.data.shopLocales;
 
-  return json({ languages });
+  return json({
+    languages,
+    primaryLanguage: languages.find((language) => language.primary),
+  });
 };
 
 export default function Settings() {
   const { languages } = useLoaderData<typeof loader>();
-  console.log({ languages });
+
+  const fetcher = useFetcher();
+  const isSubmitting = fetcher.state === "submitting";
+
+  const fieldsDefinition = languages.map((language) => ({
+    key: language.locale,
+    name: "",
+    description: "",
+  }));
+  const [selected, setSelected] = useState(0);
+
+  const handleTabChange = useCallback(
+    (selectedTabIndex: number) => setSelected(selectedTabIndex),
+    [],
+  );
+
+  const { fields, submit, reset } = useForm({
+    fields: {
+      translations: useList({
+        list: fieldsDefinition,
+        validates: {
+          name: (name) => (!name && `Name cannot be empty`) || undefined,
+          description: (description) =>
+            (!description && `Description cannot be empty`) || undefined,
+        },
+      }),
+    },
+    onSubmit: async (formData) => {
+      console.log("Form submitted with data:", formData);
+      fetcher.submit(formData, {
+        method: "POST",
+        encType: "application/json",
+      });
+      return { status: "success" };
+    },
+  });
+
+  function translationForm(
+    language: (typeof languages)[number],
+    index: number,
+  ) {
+    return (
+      <>
+        <TextField
+          label="Store name"
+          {...fields.translations[index].name}
+          autoComplete="off"
+        />
+        <TextField
+          label="Store description"
+          {...fields.translations[index].description}
+          autoComplete="off"
+          multiline={4}
+        />
+      </>
+    );
+  }
+
+  const tabs = languages.map((language, index) => ({
+    id: language.locale,
+    primary: language.primary,
+    content: language.locale,
+    accessibilityLabel: language.primary
+      ? `${language.locale} (Primary)`
+      : language.locale,
+    panelID: `${language.locale}-content`,
+  }));
 
   return (
     <Page
@@ -48,84 +121,51 @@ export default function Settings() {
     >
       <Layout>
         <Layout.Section>
-          <Card>
-            <BlockStack gap="300">
-              <Text as="p" variant="bodyMd">
-                The app template comes with an additional page which
-                demonstrates how to create multiple pages within app navigation
-                using{" "}
-                <Link
-                  url="https://shopify.dev/docs/apps/tools/app-bridge"
-                  target="_blank"
-                  removeUnderline
-                >
-                  App Bridge
-                </Link>
-                .
-              </Text>
-              <Text as="p" variant="bodyMd">
-                To create your own page and have it show up in the app
-                navigation, add a page inside <Code>app/routes</Code>, and a
-                link to it in the <Code>&lt;NavMenu&gt;</Code> component found
-                in <Code>app/routes/app.jsx</Code>.
-              </Text>
-            </BlockStack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section variant="oneThird">
-          <Card>
-            <BlockStack gap="200">
+          <Form data-save-bar onSubmit={submit}>
+            <Card>
               <Text as="h2" variant="headingMd">
-                Resources
+                Translations
               </Text>
-              <List>
-                <List.Item>
-                  <Link
-                    url="https://shopify.dev/docs/apps/design-guidelines/navigation#app-nav"
-                    target="_blank"
-                    removeUnderline
+              <Tabs tabs={tabs} selected={selected} onSelect={handleTabChange}>
+                <Box paddingBlockStart="200">
+                  <FormLayout key={selected}>
+                    {translationForm(languages[selected], selected)}
+                  </FormLayout>
+                  {/* <TextField
+                          label="Store name"
+                          {...fields[selectedLanguage.id].name}
+                          autoComplete="off"
+                        />
+                        <TextField
+                          label="Store description"
+                          {...fields[selectedLanguage.id].description}
+                          autoComplete="off"
+                          multiline={4}
+                        /> */}
+                  {/* Add more form fields */}
+                  </Box>
+              </Tabs>
+              <PageActions
+                primaryAction={
+                  <Button
+                    variant="primary"
+                    submit
+                    loading={isSubmitting}
+                    disabled={isSubmitting}
                   >
-                    App nav best practices
-                  </Link>
-                </List.Item>
-              </List>
-            </BlockStack>
-          </Card>
-        </Layout.Section>
-        <Layout.Section>
-          <Card>
-            <BlockStack gap="200">
-              <Text as="h2" variant="headingMd">
-                Setup your store default translations
-              </Text>
-              <List>
-                {languages.map((language) => (
-                  <List.Item key={language.locale}>
-                    {language.locale}
-                  </List.Item>
-                ))}
-              </List>
-            </BlockStack>
-          </Card>
+                    Save Settings
+                  </Button>
+                }
+                secondaryActions={[
+                  {
+                    content: "Cancel",
+                    onAction: reset,
+                  },
+                ]}
+              />            </Card>
+          </Form>
         </Layout.Section>
       </Layout>
     </Page>
-  );
-}
-
-function Code({ children }: { children: React.ReactNode }) {
-  return (
-    <Box
-      as="span"
-      padding="025"
-      paddingInlineStart="100"
-      paddingInlineEnd="100"
-      background="bg-surface-active"
-      borderWidth="025"
-      borderColor="border"
-      borderRadius="100"
-    >
-      <code>{children}</code>
-    </Box>
   );
 }
