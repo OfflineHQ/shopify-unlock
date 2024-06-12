a6df2e6d511e915d9693a63785506b34973be789
sebpalluel <sebpalluel@gmail.com>
2024-06-12 15:50:31 +0200

    ✨ (TargetProductsOrCollections.tsx): add new component for product selection using Shopify's app bridge 🐛 (app.create-campaign.tsx): fix title of campaign creation page from "Offline Gate" to "Campaign" ✨ (app.create-campaign.tsx): enable product validation and replace commented component with TargetProductsOrCollections ⬆️ (package.json): upgrade @shopify/polaris-icons to v9.1.1 for enhanced UI icons support

---
+++ app/components/TargetProductsOrCollections.tsx
@@ -0,0 +1,27 @@
+import { useAppBridge } from "@shopify/app-bridge-react";
+import { Button } from "@shopify/polaris";
+import { useCallback } from "react";
+
+export const TargetProductsOrCollections = ({ products }) => {
+  const shopify = useAppBridge();
+  const selectProducts = useCallback(async () => {
+    console.log({ products:products.value });
+    const updatedSelection = await shopify.resourcePicker({
+      type: "product",
+      multiple: true,
+      action: "select",
+      selectionIds: products.value.map((product) => ({ id: product.id, variants: product.variants })),
+    });
+
+    if (updatedSelection) {
+      console.log({ updatedSelection });
+      products.onChange(updatedSelection);
+    }
+  }, [shopify, products]);
+  return (
+    <Button onClick={selectProducts}>
+      Select Products
+    </Button>
+  );
+};
+
