8ca3cf7f3f5eb1e232bf2d3cd15dfe71048131c0
sebpalluel <sebpalluel@gmail.com>
2024-06-14 18:27:23 +0200

    ✨ Add new SelectedProductsTable component for better product selection management ✨ Enhance TargetProductsOrCollections component with collection selection and state management ♻️ Refactor CreateTokengate component to integrate product and collection selection logic 🔧 Update application URL and redirect URLs in shopify.app.development.toml for consistency

--- app/libs/campaigns-product-collection/TargetProductsOrCollections.tsx
+++ app/libs/campaigns-product-collection/TargetProductsOrCollections.tsx
@@ -1,8 +1,19 @@
 import { useAppBridge } from "@shopify/app-bridge-react";
-import { Button } from "@shopify/polaris";
+import {
+  BlockStack,
+  Box,
+  Button,
+  ButtonGroup,
+  Card,
+  Divider,
+  InlineGrid,
+  Text,
+} from "@shopify/polaris";
 import type { Field } from "@shopify/react-form";
-import { useCallback } from "react";
+import type { Dispatch, SetStateAction } from "react";
+import { useCallback, useEffect, useState } from "react";
 import type { Collection, Product } from "~/types/admin.types";
+import SelectedProductsTable from "./SelectedProductsTable";
 
 interface ProductField extends Field<Product[]> {
   value: Product[];
@@ -13,13 +24,21 @@ interface CollectionField extends Field<Collection[]> {
 interface TargetProductsOrCollectionsProps {
   products: ProductField;
   collections: CollectionField;
+  isProductSelection: boolean | null;
+  setIsProductSelection: Dispatch<SetStateAction<boolean | null>>;
 }
 
 export const TargetProductsOrCollections = ({
   products,
   collections,
+  isProductSelection,
+  setIsProductSelection,
 }: TargetProductsOrCollectionsProps) => {
   const shopify = useAppBridge();
+  const [selected, setSelected] = useState<string[]>(
+    products.value.map((product) => product.id),
+  );
+
   const selectProducts = useCallback(async () => {
     console.log({ products: products.value });
     const updatedSelection = await shopify.resourcePicker({
@@ -35,9 +54,108 @@ export const TargetProductsOrCollections = ({
     });
 
     if (updatedSelection) {
+      console.log("Updated selection", updatedSelection);
       // @ts-ignore
       products.onChange(updatedSelection);
+      setIsProductSelection(true);
+      setSelected(updatedSelection.map((product: Product) => product.id));
     }
-  }, [shopify, products]);
-  return <Button onClick={selectProducts}>Select Products</Button>;
+  }, [shopify, products, setIsProductSelection]);
+
+  const selectCollections = useCallback(async () => {
+    console.log({ collections: collections.value });
+    const updatedSelection = await shopify.resourcePicker({
+      type: "collection",
+      multiple: true,
+      action: "select",
+      selectionIds: collections.value.map((collection) => ({
+        id: collection.id,
+      })),
+    });
+    if (updatedSelection) {
+      // @ts-ignore
+      collections.onChange(updatedSelection);
+      setIsProductSelection(false);
+    }
+  }, [shopify, collections, setIsProductSelection]);
+
+  useEffect(() => {
+    if (products.value.length > 0 || collections.value.length > 0) {
+      setIsProductSelection(products.value.length > 0);
+    }
+  }, [products.value, collections.value, setIsProductSelection]);
+
+  function clearSelection() {
+    console.log("Clearing selection");
+    products.reset();
+    collections.reset();
+    setIsProductSelection(null);
+    setSelected([]);
+  }
+
+  return (
+    <Card roundedAbove="sm" background="bg-surface-secondary">
+      <BlockStack gap="200">
+        <InlineGrid columns="1fr auto">
+          <Text as="h3" variant="headingLg">
+            Target Products or Collections
+          </Text>
+          {isProductSelection === null ? (
+            <ButtonGroup>
+              <Button variant="plain" onClick={selectCollections}>
+                Select Collections
+              </Button>
+              <Button variant="plain" onClick={selectProducts}>
+                Select Products
+              </Button>
+            </ButtonGroup>
+          ) : (
+            <ButtonGroup>
+              <Button
+                variant="plain"
+                onClick={
+                  isProductSelection ? selectProducts : selectCollections
+                }
+              >
+                {isProductSelection ? "Add Products" : "Add Collections"}
+              </Button>
+              <Button variant="plain" onClick={clearSelection}>
+                Clear
+              </Button>
+            </ButtonGroup>
+          )}
+        </InlineGrid>
+      </BlockStack>
+      <Box paddingBlockStart="200" paddingBlockEnd="400">
+        <Text as="p" variant="bodyLg" tone="subdued">
+          Select the products or collections that will be targeted for those
+          perks in your campaign.
+          <br />
+          <br />
+          Please note that you cannot have the same product or collection
+          selected twice in the same campaign.
+        </Text>
+      </Box>
+      <Divider />
+      <Box paddingBlockStart="400" paddingBlockEnd="400">
+        {isProductSelection === null ? (
+          <InlineGrid gap="800" columns={2}>
+            <Button onClick={selectCollections}>Select Collections</Button>
+            <Button variant="primary" onClick={selectProducts}>
+              Select Products
+            </Button>
+          </InlineGrid>
+        ) : (
+          isProductSelection &&
+          products.value.length > 0 && (
+            <SelectedProductsTable
+              products={products.value}
+              selected={selected}
+              setSelected={setSelected}
+            />
+          )
+        )}
+      </Box>
+    </Card>
+  );
 };
