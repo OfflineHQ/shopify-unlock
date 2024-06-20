d125c066774ff3a402ac79fefd9b86c19d40f00e
sebpalluel <sebpalluel@gmail.com>
2024-06-20 16:03:20 +0200

    ✨ (public-api.connect.ts): add request object to loader function to log request method ♻️ (gate.js): refactor API endpoint paths to include 'public-api' for clarity and consistency

--- app/routes/public-api.connect.ts
+++ app/routes/public-api.connect.ts
@@ -1,7 +1,8 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { json } from "@remix-run/node";
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  console.log({ method: request.method });
   // Your API logic goes here
   const data = {
     message: "Hello from the public API!",
