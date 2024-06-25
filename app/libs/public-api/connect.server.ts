12584004121bc16aa19c4da77b54407f14527265
sebpalluel <sebpalluel@gmail.com>
2024-06-25 16:37:48 +0200

    ♻️ Refactor connect and evaluateGate functions to use shared verification logic ✨ Add shared-functions.server.ts to centralize common functionality across modules 💡 Update comments and clean up code for better readability and maintenance

--- app/libs/public-api/connect.server.ts
+++ app/libs/public-api/connect.server.ts
@@ -1,65 +1,65 @@
 import { json } from "@remix-run/node";
 import createLinkedCustomer from "./proxy-request/create-linked-customer.server";
-import verifySignatureWithCometh from "./proxy-request/verify-signature-with-cometh.server";
 import type { ConnectArgsSchema } from "./schema";
+import {
+  getVaultsPayload,
+  verifySignatureAndCustomerId,
+} from "./shared-functions.server";
 
-// TODO: connect without `ProductGateRequest` (because we would like to put a connection block into section and not only on page)
+export default async function connect(args: ConnectArgsSchema) {
+  const {
+    address,
+    customerId,
+    existingCustomer,
+    shopDomain,
+    gateConfigurationGid,
+    productGid,
+  } = args;
 
-export default async function connect({
-  address,
-  message,
-  signature,
-  shopDomain,
-  productGid,
-  gateConfigurationGid,
-  customerId,
-  existingCustomer,
-}: ConnectArgsSchema) {
-  if (!existingCustomer || !customerId || !address || !message || !signature) {
+  if (!existingCustomer || !customerId) {
     return json({ message: "Invalid request" }, { status: 403 });
   }
 
-  const existingCutomerAddress = existingCustomer.address?.toLowerCase() || "";
+  const existingCustomerAddress = existingCustomer.address?.toLowerCase() || "";
 
   if (
-    existingCutomerAddress &&
-    existingCutomerAddress !== address.toLowerCase()
+    existingCustomerAddress &&
+    existingCustomerAddress !== address.toLowerCase()
   ) {
     return json({ message: "Invalid address" }, { status: 403 });
   }
 
-  try {
-    const isValidSignature = await verifySignatureWithCometh({
-      address,
-      message,
-      signature,
-    });
-    if (!isValidSignature) {
-      return json({ message: "Invalid signature" }, { status: 403 });
-    }
-    // the message signed is the customerId
-    else if (message !== customerId) {
-      return json({ message: "Invalid customerId" }, { status: 403 });
-    }
-    if (!existingCutomerAddress) {
-      await createLinkedCustomer({
-        customerId,
-        address: address.toLowerCase(),
-        shopDomain,
-      });
-    }
-    const payload = {
-      linkedCustomer: {
-        address,
-      },
-      walletAddress: address,
-      walletVerificationMessage: message,
-      walletVerificationSignature: signature,
-    };
-    // TODO, in the context of connect with the product and gate also evaluate and return the vaults with hmac signature
-    return json(payload, { status: 200 });
-  } catch (error) {
-    console.error("Signature verification failed:", error);
-    return json({ message: "Failed to verify signature" }, { status: 500 });
+  const verificationResult = await verifySignatureAndCustomerId(args);
+  if (verificationResult) {
+    return verificationResult;
   }
+
+  if (!existingCustomerAddress) {
+    await createLinkedCustomer({
+      customerId,
+      address: address.toLowerCase(),
+      shopDomain,
+    });
+  }
+
+  const vaultsPayload =
+    gateConfigurationGid && productGid
+      ? await getVaultsPayload({
+          gateConfigurationGid,
+          shopDomain,
+          productGid,
+        })
+      : {};
+
+  const payload = {
+    linkedCustomer: {
+      address,
+    },
+    walletAddress: address,
+    walletVerificationMessage: args.message,
+    walletVerificationSignature: args.signature,
+    ...vaultsPayload,
+  };
+
+  return json(payload, { status: 200 });
 }
--- app/libs/public-api/evaluate-gate.ts
+++ app/libs/public-api/evaluate-gate.ts
@@ -1,55 +1,16 @@
 import { json } from "@remix-run/node";
-import getHmac from "./get-hmac.server";
-import verifySignatureWithCometh from "./proxy-request/verify-signature-with-cometh.server";
 import type { EvaluateGateArgsSchema } from "./schema";
+import {
+  getVaultsPayload,
+  verifySignatureAndCustomerId,
+} from "./shared-functions.server";
 
-// TODO: connect without `ProductGateRequest` (because we would like to put a connection block into section and not only on page)
+export default async function evaluateGate(args: EvaluateGateArgsSchema) {
+  const verificationResult = await verifySignatureAndCustomerId(args);
+  if (verificationResult) {
+    return verificationResult;
+  }
 
-export default async function evaluateGate({
-  address,
-  message,
-  signature,
-  shopDomain,
-  productGid,
-  gateConfigurationGid,
-  customerId,
-}: EvaluateGateArgsSchema) {
-  if (!address || !message || !signature) {
-    return json({ message: "Invalid request" }, { status: 403 });
-  }
-  try {
-    const isValidSignature = await verifySignatureWithCometh({
-      address,
-      message,
-      signature,
-    });
-    if (!isValidSignature) {
-      return json({ message: "Invalid signature" }, { status: 403 });
-    }
-    // the message signed is the customerId
-    else if (message !== customerId) {
-      return json({ message: "Invalid customerId" }, { status: 403 });
-    }
-    //TODO, get gate metadata with product
-    // const requiredContractAddresses = await getProductGate({
-    //   shopDomain,
-    //   productGid,
-    //   gatesHandle: process.env.OFFLINE_GATES_HANDLE,
-    // });
-    //TODO, for now we return a validated gate because we don't filter yet with segments etc.
-    const payload = {
-      vaults: [
-        {
-          ...getHmac(gateConfigurationGid),
-          isLoyaltyCard: true,
-          canMint: false,
-        },
-      ],
-    };
-    // TODO, in the context of connect with the product and gate also evaluate and return the vaults with hmac signature
-    return json(payload, { status: 200 });
-  } catch (error) {
-    console.error("Signature verification failed:", error);
-    return json({ message: "Failed to verify signature" }, { status: 500 });
-  }
+  const payload = await getVaultsPayload(args);
+  return json(payload, { status: 200 });
 }
